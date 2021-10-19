fbfbefe7f26fe62b263232f5758fb7c1861a92d7
sunfanyi <sunfanyi_jim@outlook.com>
2021-10-19 09:28:18 +0100

    __init__ and display the software counter value on the LED array in binary (Exercise 1.1)

--- LEDarray.c
+++ LEDarray.c
@@ -7,8 +7,26 @@
 ************************************/
 void LEDarray_init(void)
 {
-    	//set up TRIS registers for pins connected to LED array
+    //set up TRIS registers for pins connected to LED array
+    TRISGbits.TRISG0 = 0;
+    TRISGbits.TRISG1 = 0;
+    TRISAbits.TRISA2 = 0;
+    TRISFbits.TRISF6 = 0;
+    TRISAbits.TRISA4 = 0;
+    TRISAbits.TRISA5 = 0;
+    TRISFbits.TRISF0 = 0;
+    TRISBbits.TRISB0 = 0;
+    
 	//set initial output LAT values (they may have random values when powered on)
+    // (rand() % 3) - 1  // random value (0 or 1)
+    LATGbits.LATG0 = 0;  
+    LATGbits.LATG1 = 0;
+    LATAbits.LATA2 = 0;
+    LATFbits.LATF6 = 0;
+    LATAbits.LATA4 = 0;
+    LATAbits.LATA5 = 0;
+    LATFbits.LATF0 = 0;
+    LATBbits.LATB0 = 0;
 }
 
 /************************************
@@ -19,6 +37,14 @@ void LEDarray_disp_bin(unsigned int number)
 {
 	//some code to turn on/off the pins connected to the LED array
 	//if statements and bit masks can be used to determine if a particular pin should be on/off
+    if (number & 0b00000001) {LATGbits.LATG0 = 1;} else {LATGbits.LATG0 = 0;}
+    if (number & 0b00000010) {LATGbits.LATG1 = 1;} else {LATGbits.LATG1 = 0;}
+    if (number & 0b00000100) {LATAbits.LATA2 = 1;} else {LATAbits.LATA2 = 0;}
+    if (number & 0b00001000) {LATFbits.LATF6 = 1;} else {LATFbits.LATF6 = 0;}
+    if (number & 0b00010000) {LATAbits.LATA4 = 1;} else {LATAbits.LATA4 = 0;}
+    if (number & 0b00100000) {LATAbits.LATA5 = 1;} else {LATAbits.LATA5 = 0;}
+    if (number & 0b01000000) {LATFbits.LATF0 = 1;} else {LATFbits.LATF0 = 0;}
+    if (number & 0b10000000) {LATBbits.LATB0 = 1;} else {LATBbits.LATB0 = 0;}
 	//see Readme.md for examples
 }
 
